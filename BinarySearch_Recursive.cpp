b86120acc07080ada607d63b05b2ce0aabe2ab84
alina-babayan <alina.babayan2@edu.ysu.am>
2025-05-31 12:45:19 +0400

    Update BinarySearch_Recursive.cpp

--- BinarySearch_Recursive.cpp
+++ BinarySearch_Recursive.cpp
@@ -1,40 +1,34 @@
-// #include <iostream>
-// #include <vector>
+#include <iostream>
 
-// template <typename C, typename T>
-// auto MyBinarySearch(const C& cont, const T& target, int l, int r)
-// {
-//     if(l > r) return -1;
-//     int mid = l + (r-1)/2;
-//     if(*mid == target)
-//     {
-//         return mid;
-//     }
-//     else if(*mid < target)
-//     {
-//         return MyBinarySearch(cont,target,mid + 1,r);
-//     }
-//     else
-//     {
-//         return MyBinarySearch(cont,target,l,mid - 1);
+int MyBinarySearch(int arr[],int target, int l, int r)
+{
+    if(l > r) return -1;
+    int mid = l + (r-l)/2;
+    if(arr[mid] == target)
+    {
+        return mid;
+    }
+    else if(arr[mid]  < target)
+    {
+        return MyBinarySearch(arr,target,mid + 1,r);
+    }
+    else
+    {
+        return MyBinarySearch(arr,target,l,mid - 1);
 
-//     }
-// }
+    }
+}
 
-// int main()
-// {
-//     std::vector<int> vec = {1,5,8,9,15};
-//     auto l = vec.begin();
-//     auto r = vec.end();
-
-//     auto it = MyBinarySearch(vec,4,l,r);
-//     if(it != vec.end())
-//     {
-//         std::cout<<*it<<std::endl;
-//     }
-//     else{
-//         std::cout<<"not found\n";
-//     }
-//     return 0;
-
-// }
+int main()
+{
+    int n = 6;
+    int arr[n] = {1,2,3,4,5,6};
+    int t = 4;
+    int it = MyBinarySearch(arr,t,0,n-1);
+   if(it != -1 )
+   {
+    std::cout<< t <<std::endl;
+   }
+   
+    return 0;
+}
